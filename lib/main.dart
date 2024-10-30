99c8bac7c9309aa7c675a648803c99ac7fc18d04
eskokado <eskokado@gmail.com>
2024-10-30 05:12:44 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text('Versão inicial'),
+      ),
+    );
+  }
+}
