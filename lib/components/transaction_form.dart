4635e4a69f9b43c504200a7d3c43f577866c6ac8
eskokado <eskokado@gmail.com>
2024-11-02 08:49:34 -0300

    feat(form): separando widget

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm({super.key, required this.onSubmit});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Título',
+              ),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(
+                  labelText: 'Valor (R\$)'
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      final title = titleController.text;
+                      final value = double.tryParse(valueController.text) ?? 0.0;
+
+                      onSubmit(title, value);
+                    },
+                    child: Text(
+                      'Nova Transação',
+                      style: TextStyle(
+                          color: Colors.purple
+                      ),
+                    )
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
